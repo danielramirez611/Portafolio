7b5b4e0208f2a05e58e4b469cf3ad5a448ecca23
Daniel Ramirez <123519249+danielramirez611@users.noreply.github.com>
2025-05-11 22:52:55 -0500

    Update create_superuser.py

--- django_portfolio/create_superuser.py
+++ django_portfolio/create_superuser.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
 # Configura Django para que puedas usar sus modelos
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio.settings')
 django.setup()
 
 # Obtener el modelo de usuario
